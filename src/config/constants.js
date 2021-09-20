ab4e906557d58e394f110976e6a2ef1e840f7af9
gang-min <rudals8920@naver.com>
2021-09-20 20:53:50 +0900

    api URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://gangmin-market-server.herokuapp.com";
+export const API_URL = process.env.NODE_ENV === 'production' ? "https://gangmin-market-server.herokuapp.com" : "http://localhost:8080" ;
